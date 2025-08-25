035fe4ad24710e4ea1e253528306b54eaa18cb1c
Gabriel Ribeiro <gabriel.ribeiro@impar.com.br>
2025-08-24 23:12:05 -0300

    creating education materials section

--- index.js
+++ index.js
@@ -1,3 +1,173 @@
+const pdfData = [
+  {
+    id: 1,
+    title: "Protocolo de Acolhimento Terapêutico para Crianças Neurodivergentes: Criando Conexões Significativas",
+    description: "Este protocolo visa aprimorar o acolhimento e a intervenção terapêutica para crianças neurodivergentes, promovendo um ambiente seguro e eficaz para o desenvolvimento",
+    thumbnail: "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/thumbnail.jpg",
+    slides: [
+      "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/page_1.jpg",
+      "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/page_2.jpg",
+      "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/page_3.jpg",
+      "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/page_4.jpg",
+      "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/page_5.jpg",
+      "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/page_6.jpg",
+      "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/page_7.jpg",
+      "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/page_8.jpg",
+      "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/page_9.jpg",
+      "./assets/Protocolo-de-Acolhimento-Terapeutico-para-Criancas-Neurodivergentes-Criando_20250822_131543_0000/page_10.jpg"
+    ],
+    pages: 10
+  },
+  {
+    id: 2,
+    title: "Comportamentos Disruptivos na Escola: Compreendendo para Intervir",
+    description: "Todo comportamento é uma forma de comunicação - o olhar atento e acolhedor do educador pode ser a chave para transformar a vida e o ambiente escolar de todos os alunos",
+    thumbnail: './assets/DOC-20250715-WA0022/thumbnail.jpg',
+    slides: [
+      './assets/DOC-20250715-WA0022/page_1.jpg',
+      './assets/DOC-20250715-WA0022/page_2.jpg',
+      './assets/DOC-20250715-WA0022/page_3.jpg',
+      './assets/DOC-20250715-WA0022/page_4.jpg',
+      './assets/DOC-20250715-WA0022/page_5.jpg',
+      './assets/DOC-20250715-WA0022/page_6.jpg',
+      './assets/DOC-20250715-WA0022/page_7.jpg',
+      './assets/DOC-20250715-WA0022/page_8.jpg',
+      './assets/DOC-20250715-WA0022/page_9.jpg',
+      './assets/DOC-20250715-WA0022/page_10.jpg',
+    ],
+    pages: 10
+  },
+  {
+    id: 3,
+    title: "Neuroeducação: Entendendo como o Cérebro Aprende",
+    description: "Explorando as bases neurológicas que transformam a aprendizagem",
+    thumbnail: "./assets/DOC-20250707-WA0019/thumbnail.jpg",
+    slides: [
+      './assets/DOC-20250707-WA0019/page_1.jpg',
+      './assets/DOC-20250707-WA0019/page_2.jpg',
+      './assets/DOC-20250707-WA0019/page_3.jpg',
+      './assets/DOC-20250707-WA0019/page_4.jpg',
+      './assets/DOC-20250707-WA0019/page_5.jpg',
+      './assets/DOC-20250707-WA0019/page_6.jpg',
+      './assets/DOC-20250707-WA0019/page_7.jpg',
+      './assets/DOC-20250707-WA0019/page_8.jpg',
+      './assets/DOC-20250707-WA0019/page_9.jpg',
+      './assets/DOC-20250707-WA0019/page_10.jpg',
+      './assets/DOC-20250707-WA0019/page_11.jpg',
+    ],
+    pages: 11
+  },
+  {
+    id: 4,
+    title: "Desafios e Compromissos na Educação Inclusiva",
+    description: "Navegar pelas complexidades da educação inclusiva exige dedicação e uma compreensão profunda das necessidades de cada aluno. Esta apresentação explodirá os pilares que sustentam uma prática pedagógica verdadeiramente inclusiva.",
+    thumbnail: "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/thumbnail.jpg",
+    slides: [
+      "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/page_1.jpg",
+      "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/page_2.jpg",
+      "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/page_3.jpg",
+      "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/page_4.jpg",
+      "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/page_5.jpg",
+      "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/page_6.jpg",
+      "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/page_7.jpg",
+      "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/page_8.jpg",
+      "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/page_9.jpg",
+      "./assets/Desafios-e-Compromissos-na-Educacao-Inclusiva (1).pptx_20250823_192434_0000/page_10.jpg"
+    ],
+    pages: 10
+  }
+]
+
+const cardContainer = document.querySelector('.educational-materials');
+
+function renderCards() {
+  pdfData.forEach(card => {
+    const div = document.createElement("div");
+    div.className = "pdf-card";
+    div.innerHTML = `
+      <div class="card-header">
+        <span class="pages">${card.pages} páginas</span>
+      </div>
+      <div class="card-image">
+        <img src="${card.thumbnail}" alt="${card.title}" />
+      </div>
+      <div class="card-body">
+        <h3>${card.title}</h3>
+        <p>${card.description}</p>
+      </div>
+      <div class="card-footer">
+        <button onclick="openModal(${card.id})">Ver mais</button>
+      </div>
+    `;
+    cardContainer.appendChild(div);
+  })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderCards();
+});
+
+let currentSlides = [];
+let currentSlide = 0;
+
+function openModal(id) {
+  const card = pdfData.find(c => c.id === id);
+
+  if(!card) return;
+
+  document.getElementById("modal-title").textContent = card.title;
+  currentSlides = card.slides;
+  currentSlide = 0;
+  updateSlider();
+  document.getElementById("modal").classList.remove("modal-hidden");
+}
+
+function closeModal() {
+  const modal = document.getElementById("modal")
+  modal.classList.add("modal-hidden");
+}
+
+function updateSlider() {
+  const img = document.getElementById("slide-image");
+  img.src = currentSlides[currentSlide];
+  const counterEl = document.getElementById("slide-counter");
+
+ if(counterEl) {
+  counterEl.textContent = `${currentSlide + 1} / ${currentSlides.length}`;
+ } 
+
+ const thumbContainer = document.getElementById("thumbnail-row");
+ thumbContainer.innerHTML = "";
+
+ currentSlides.forEach((src, index) => {
+  const thumb = document.createElement("img");
+  thumb.src = src;
+  
+  if(index === currentSlide) thumb.classList.add('active');
+
+  thumb.onclick = () => {
+    currentSlide = index;
+    updateSlider();
+  }
+
+  thumbContainer.appendChild(thumb);
+ })
+}
+
+function nextSlide() {
+  if(currentSlide < currentSlides.length - 1){
+    currentSlide++;
+    updateSlider();
+  }
+}
+
+function prevSlide() {
+  if(currentSlide > 0) {
+    currentSlide--;
+    updateSlider();
+  }
+}
+
 function sendMessage() {
   const isMobile = /iPhone|Android|iPad|iPod|Mobile/i.test(navigator.userAgent);
 
@@ -84,5 +254,4 @@ function toggleCertifications() {
     toggleBtn.textContent = "Ver menos";
   }
 }
-
 
